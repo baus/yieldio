1e3242b5b3f5f2a5bb2e11982001c364d26e6800
chris baus <christopher@baus.net>
2018-01-17 13:34:02 -0800

    Remove commented out code

--- server/lib/routes.js
+++ server/lib/routes.js
@@ -1,10 +1,7 @@
-'use strict';
-
 const data = require('./data');
 const express = require('express');
 
 module.exports = function(app) {
     app.use(express.static('server/static'));
     app.get('/api/allyields.json', (req, res) => res.json(data.YieldSpread));
-    /*app.get('/', (req, res) => res.sendfile('react'));*/
 };
