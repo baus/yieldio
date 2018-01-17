0226beae6dab5edf1b68df1c7103bb4a81cb48bf
chris baus <christopher@baus.net>
2018-01-17 14:12:09 -0800

    Add comment

--- util/cleanyields.js
+++ util/cleanyields.js
@@ -1,3 +1,7 @@
+/**
+ *  This script removes unrounded values and NaN's from the allYieldsArray.json
+ */
+
 const fs = require('fs');
 const contents = fs.readFileSync('allyieldsarray.json');
 const allYieldsArray = JSON.parse(contents);
@@ -5,7 +9,9 @@ const allYieldsArray = JSON.parse(contents);
 allYieldsArray.forEach(dateArray=>dateArray
 		       .slice(1).forEach(durationArray=>durationArray
 					 .forEach((durationValue, index, array)=>{
-					     !durationValue || durationValue === 'NaN'? array[index] = null : array[index]=Math.round(100 * durationValue) / 100
+					     !durationValue || durationValue === 'NaN'?
+                             array[index] = null :
+                             array[index] = (Math.round(100 * durationValue) / 100);
 					 })));
 fs.writeFileSync('output.json', JSON.stringify(allYieldsArray));
 
