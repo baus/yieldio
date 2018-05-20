1db6e988fa3cf3b2464268aad3316d78b45a5a4b
chris baus <christopher@baus.net>
2018-05-19 21:39:33 -0400

    Address issue #2

--- client/src/yieldcallout.js
+++ client/src/yieldcallout.js
@@ -17,11 +17,11 @@ const YieldCallout = props => {
     return (
         <Panel loading={!props.yieldsForDate}>
             <div className="row">
-                <div className="col-sm-8">
+                <div className="col-sm-7">
                     <span className="the-number"><CalloutValue text={callout} bpsChange={bpsChange}/></span><br></br>
                     {durationLabel} yield as of <FormattedDate date={props.currentDate}/>
                 </div>
-                <div className="col-sm-4 callout">
+                <div className="col-sm-5 callout">
                     <div><span>{bpsCallout}</span> bps</div>
                     <div>% change: <span>{percentageChangeCallout}</span></div>
                 </div>
