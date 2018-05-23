dc6de4412e55a7607bed5f4a2d2792a64a072e16
chris baus <christopher@baus.net>
2018-05-23 14:55:08 -0400

    Remove unused imports

--- client/src/about.js
+++ client/src/about.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody } from 'reactstrap';
 
 class About extends React.Component {
     constructor(props) {
